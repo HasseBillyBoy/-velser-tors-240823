b3603a0315ce8bfb35fda10051108818683f1db9
Hasse <haje0001@stud.kea.dk>
2023-08-24 10:17:41 +0200

    tilføjet js

---
+++ js/js.js
@@ -0,0 +1,14 @@
+let titel = document.querySelector("h1");
+console.log(titel);
+
+let underoverskrift = document.querySelector("h2");
+console.log(underoverskrift);
+
+let artikel = document.querySelector("h3");
+console.log(artikel);
+
+let paragraf = document.querySelector("p");
+console.log(paragraf);
+
+let billede = document.querySelector("img");
+console.log(billede);
