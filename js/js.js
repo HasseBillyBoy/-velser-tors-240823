f6c286db6e78aad67d099d0deff30a05ef09dd72
Hasse <haje0001@stud.kea.dk>
2023-08-24 10:52:47 +0200

    eureka med chaptgpt

--- js/js.js
+++ js/js.js
@@ -1,14 +1,30 @@
 let titel = document.querySelector("h1");
 console.log(titel);
 
+titel.textContent = "Noget andet";
+
 let underoverskrift = document.querySelector("h2");
 console.log(underoverskrift);
 
-let artikel = document.querySelector("h3");
-console.log(artikel);
-
-let paragraf = document.querySelector("p");
-console.log(paragraf);
-
 let billede = document.querySelector("img");
 console.log(billede);
+
+let artikel2paragraftekst = document.querySelector(
+  ".articlegrid div:nth-child(2) p"
+);
+artikel2paragraftekst.textContent = "Oookay";
+
+let artikel1paragraftekst = document.querySelector(
+  ".articlegrid div:first-child p"
+);
+artikel1paragraftekst.textContent = "Okayokay";
+
+let artikeloverskrift1 = document.querySelector(
+  ".articlegrid div:first-child h3"
+);
+artikeloverskrift1.textContent = "Splitteminebramsegl";
+
+let artikeloverskrift2 = document.querySelector(
+  ".articlegrid div:last-child h3"
+);
+artikeloverskrift2.textContent = "Spytpåmig";
